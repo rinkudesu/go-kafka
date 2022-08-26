2af30c0fbe70beebbfa8317d6bd053c3976c1d20
Piotr Kowalski <piotr@pkowalski.net>
2022-08-26 22:31:34 +0200

    Add docs

--- producer/Producer.go
+++ producer/Producer.go
@@ -1,6 +1,9 @@
 package producer
 
+// Producer is responsible for sending messages over to the message broker
 type Producer interface {
+	// Produce sends a message to the broker with the given topic
 	Produce(topic string, message any) error
+	// Close closes connection and frees any used resources
 	Close()
 }
--- subscriber/Subscriber.go
+++ subscriber/Subscriber.go
@@ -1,11 +1,18 @@
 package subscriber
 
+// Subscriber is a consumer for a message broker. It will listen for new messages from the broker and pass them for handling.
 type Subscriber interface {
+	// Subscribe subscribed to a topic defined in handler.
 	Subscribe(handler MessageHandler) error
+	// Unsubscribe removes current subscription.
 	Unsubscribe() error
 
+	// BeginHandle starts handling messages received from the broker.
+	// This call starts a goroutine in the background and will not block.
 	BeginHandle() error
+	// StopHandle stops message handling and blocks until current message processing finishes.
 	StopHandle()
 
+	// Close disables handling, unsubscribed, and disconnects from the broker.
 	Close() error
 }
